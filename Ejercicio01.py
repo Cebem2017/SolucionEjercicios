5169457f52a07e06525e66bb0ca42120fc53919e
davidpkami <davidpkami@gmail.com>
2017-11-22 09:09:21 +0100

    Modificacion

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -2,3 +2,11 @@ fichero = open("fichero.txt",'r')
 fleido=fichero.read()
 fseparado = fleido.split(";") 
 print (fseparado)
+
+num=0
+while num < len(fseparado):
+    if((fseparado[num]%2)==0):
+        print(fseparado[num]+"Es Par")
+        
+    else:
+        print(fseparado[num]+"No es Par")
