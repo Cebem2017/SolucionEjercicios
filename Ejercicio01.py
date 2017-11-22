1ad97d574c2dce6580f7e068a76d4dcc290ac837
davidpkami <davidpkami@gmail.com>
2017-11-22 09:13:49 +0100

    Confimrado

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -3,10 +3,15 @@ fleido=fichero.read()
 fseparado = fleido.split(";") 
 print (fseparado)
 
+cont=0
 num=0
 while num < len(fseparado):
     if((fseparado[num]%2)==0):
         print(fseparado[num]+"Es Par")
+        cont+=1
         
     else:
         print(fseparado[num]+"No es Par")
+    num+=1
+
+print("HAY "+cont+" PARES")
