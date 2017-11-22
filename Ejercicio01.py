5477af9bfabedae5e6b06b94138da5fca17e0f2f
kant003 <gonzalezm.angel@gmail.com>
2017-11-22 09:29:50 +0100

    Cambio while por for y refactorizo codigo

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -1,17 +1,12 @@
 fichero = open("fichero.txt",'r')
-fleido=fichero.read()
-fseparado = fleido.split(";") 
-print (fseparado)
+texto_leido=fichero.read()
+lista_numeros = texto_leido.split(";") 
 
-cont=0
-num=0
-while num < len(fseparado):
-    if int(fseparado[num])%2==0:
-        #print(str(fseparado[num])+"Es Par")
-        cont+=1
-    num+=1
+cont_pares=0
+for num in lista_numeros:
+    if int(num)%2==0:
+        cont_pares+=1
 
-contador=str(cont)
-print("Hay "+contador+" numeros pares gente de clase")
+print("Hay "+str(cont_pares)+" numeros pares gente de clase")
 print("Este ejercicio ha sido ofrecido por Kant003 con la colaboracion de DAM del norte")
 fichero.close()
