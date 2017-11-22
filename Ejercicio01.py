eb1770d6bd687bd2aadbbde8ab2b44553ec32929
Alexandre Insua <alexandreinsua@gmail.com>
2017-11-22 10:10:53 +0100

    Control de excepciones

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -1,12 +1,22 @@
-fichero = open("fichero.txt",'r')
-texto_leido=fichero.read()
+try:
+    fichero = open("fichero.txt",'r')
+except IOError:
+    print('No se pudo abrir el fichero')
+texto_leido = fichero.read()
 lista_numeros = texto_leido.split(";") 
 
-cont_pares=0
+cont_pares = 0
 for num in lista_numeros:
-    if int(num)%2==0:
-        cont_pares+=1
+    try:
+        if int(num) % 2 == 0:
+            cont_pares += 1
+    except Exception:
+        print('Error en la conversión de números')
 
-print("Hay "+str(cont_pares)+" numeros pares gente de clase")
+print("Hay " + str(cont_pares) + " numeros pares gente de clase")
 print("Este ejercicio ha sido ofrecido por Kant003 con la colaboracion de DAM del norte")
-fichero.close()
+
+try:
+    fichero.close()
+except IOError:
+    print('No se pudo cerrar el fichero')
